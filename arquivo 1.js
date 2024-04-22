7d4517edc278c0f460d88b285495210034dc7c77
Mariana Marques Dantas da Silva <mariana.silva@compasso.com.br>
2024-04-22 13:54:50 -0300

    idade

---
+++ arquivo 1.js
@@ -0,0 +1,19 @@
+// let idade = 5; 
+// console.log(idade);
+//let altura = 180;
+// camelCase
+let nome = 'jhonatan';
+console.log(nome);
+let idade = 25;
+let estaAprovado = true;
+let sobrenome ="silva";
+
+
+let pessoa = {
+    nome:'Rafael'
+
+};
+
+console.log(pessoa);
+console.log (Rafael);
+console.log(sobrenome);

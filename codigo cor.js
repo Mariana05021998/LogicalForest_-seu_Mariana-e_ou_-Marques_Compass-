984b260d5eeb249c409e6a03d06ed7ccc4ecbb83
Mariana Marques Dantas da Silva <mariana.silva@compasso.com.br>
2024-04-22 14:32:21 -0300

    Cor

---
+++ codigo cor.js
@@ -0,0 +1,10 @@
+//verbo+substantivo
+let corSite="azul";
+function resetaCor(cor,tonalidade){
+    corSite=cor+tonalidade;
+
+};
+
+console.log(corSite);
+resetaCor("vermelho");
+console.log(corSite);
